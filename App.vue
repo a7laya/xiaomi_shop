7688db9780ab4bfe4d9ce42e380ca7c21d409962
a7laya <75739680@qq.com>
2019-09-30 18:26:59 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,25 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* 官方UI库 */
+	@import "/common/uni.css";
+	/* 第三方动画库 */
+	@import "/common/animate.css";
+	/* 自定义图标库 */
+	@import "/common/icon.css";
+	/* 公共样式 */
+	@import "/common/common.css";
+</style>
