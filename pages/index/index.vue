2e2563c96a0516d34cc8aa3f84e484b8ef993f60
a7laya <75739680@qq.com>
2019-10-04 13:27:39 +0800

    首页搜索栏写完

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,12 +1,17 @@
 <template>
 	<view>
-		<scroll-view scroll-x="true" >
-			<view style='width:200upx; border: 1px solid red' >1</view>
-			<view style='width:200upx; border: 1px solid red' >2</view>
-			<view style='width:200upx; border: 1px solid red' >3</view>
-			<view style='width:200upx; border: 1px solid red' >4</view>
-			<view style='width:200upx; border: 1px solid red' >5</view>
-		</scroll-view>
+		<!-- 轮播图组件 -->
+		<swiper indicator-dots autoplay :interval="3000" :duration="1000">
+			<swiper-item>
+				<view class="swiper-item"></view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item"></view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item"></view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -28,29 +33,5 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	
 </style>
